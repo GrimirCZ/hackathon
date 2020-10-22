1e2d451347f3ad4027c019295af085f5f9f87219
grimir <falta.vit@zsbcuo.cz>
2020-10-22 10:59:35 +0200

    fix search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -14,7 +14,8 @@ class SearchController extends Controller
         $connections = Connection::where(DB::raw("LOWER(REPLACE(`name`, ' ', ''))"), "like", DB::raw("LOWER(REPLACE('%$query%', ' ',''))"))
             ->join("snapshots", "snapshots.connection_id", "=", "connections.id")
             ->orderByDesc("snapshots.time")
-            ->select("snapshots.time as time", "snapshots.connection_id as con_id");
+            ->select("snapshots.time as time", "snapshots.connection_id as con_id")
+        ->distinct();
 
         return ConnectionResource::collection(
             Connection::joinSub($connections, "snp", function($join){
