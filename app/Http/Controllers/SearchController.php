71879baf74da29ad9e66656b11b6137b7cc7a321
grimir <falta.vit@zsbcuo.cz>
2020-10-22 10:53:15 +0200

    added search

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ConnectionResource;
+use App\Models\Connection;
+use Illuminate\Support\Facades\DB;
+use Request;
+
+class SearchController extends Controller
+{
+    public function __invoke($query)
+    {
+        $connections = Connection::where(DB::raw("LOWER(REPLACE(`name`, ' ', ''))"), "like", DB::raw("LOWER(REPLACE('%$query%', ' ',''))"))
+            ->join("snapshots", "snapshots.connection_id", "=", "connections.id")
+            ->orderByDesc("snapshots.time")
+            ->select("snapshots.time as time", "snapshots.connection_id as con_id");
+
+        return ConnectionResource::collection(
+            Connection::joinSub($connections, "snp", function($join){
+                $join->on("connections.id", "=", "snp.con_id");
+            })->orderByDesc("snp.time")
+                ->select("connections.*")
+                ->get()
+        );
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ConnectionController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::resource('/spojeni', ConnectionController::class);
+Route::get('spojeni/hledat/{query?}', SearchController::class);
+Route::resource('spojeni', ConnectionController::class);
