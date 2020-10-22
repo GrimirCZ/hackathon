7e541e43ae51bf6f96013754ddce2d2e3fbfca74
grimir <falta.vit@zsbcuo.cz>
2020-10-22 09:11:01 +0200

    waits for import

--- app/Imports/WaitForImport.php
+++ app/Imports/WaitForImport.php
@@ -20,7 +20,13 @@ class WaitForImport implements ToCollection, WithHeadingRow
         $this->logger = $logger;
     }
 
-    function get_data_for_connection($identifier)
+    function get_train_id($line)
+    {
+        return preg_replace("/([a-zA-Z]+)(\d+)/", "K-$2", $line);
+    }
+
+
+    function get_data_for_connection($identifier, $type)
     {
         $res = Connection::where("identifier", "=", $identifier)->first();
 
@@ -46,11 +52,11 @@ class WaitForImport implements ToCollection, WithHeadingRow
             'operator' => $res['operator']
         ];
 
-        if($res['vehicleType'] == Autobus){
+        if($type == "BUS"){
             $con_data['line_number'] = $res['lineNumber'];
             $con_data['service_number'] = $res['serviceNumber'];
             $con_data['name'] = $res['lineNumber'] . "/" . $res['serviceNumber'];
-        } else if($res['vehicleType'] == Vlak){
+        } else if($type == "Vlak"){
             $con_data['train_number'] = $res['trainNumber'];
             $con_data['name'] = $res['trainKind'] . " " . $res['trainNumber'];
         }
@@ -69,8 +75,15 @@ class WaitForImport implements ToCollection, WithHeadingRow
     {
         foreach($rows as $row){
             DB::transaction(function() use ($row){
+
+
                 $id1 = "S-" . $row['line1'] . "-" . $row['service1'];
-                $id2 = "S-" . $row['line2'] . "-" . $row['service2'];
+
+                if($row['vehicletype'] == "BUS"){
+                    $id2 = "S-" . $row['line2'] . "-" . $row['service2'];
+                } else{
+                    $id2 = $this->get_train_id($row['line2']);
+                }
 
                 if($id1 == $id2){
                     $this->logger->info("id1 = id2 (" . $id2 . ")");
@@ -80,16 +93,16 @@ class WaitForImport implements ToCollection, WithHeadingRow
                 $this->logger->info("id1: " . $id1 . " id2: " . $id2);
 
 
-                $awaits = $this->get_data_for_connection($id1);
+                $awaits = $this->get_data_for_connection($id1, "BUS");
 
-                $awaited_for = $this->get_data_for_connection($id2);
+                $awaited_for = $this->get_data_for_connection($id2, $row['vehicletype']);
 
                 if($awaits != null && $awaited_for != null &&
                     $awaits->whereHas("waits_for", function($q) use ($awaited_for){ $q->where("awaited_for_id", "=", $awaited_for->id); })->count() == 0
                 ){
 
                     $awaits->waits_for()->attach($awaited_for, [
-                        'minutes' => $row['waits_for'],
+                        'minutes' => $row['minutes'],
                         'station' => $row['stop'],
                         'created_at' => DB::raw("CURRENT_TIMESTAMP"),
                         'updated_at' => DB::raw("CURRENT_TIMESTAMP"),
